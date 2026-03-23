13ea5f65bfb566c61350de5deafd0f152f15f3fb
NikoboiNFTB <contact@nikoboi.dev>
2026-03-23 11:14:27 +0200

    mv */.old/ to dev.nikoboi.dev/*/.old

--- .old/pull/all-pull.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for d in */; do
-    (cd "$d" && ./pull.sh) &
-done
-
-wait
--- .old/push/all-push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for d in */; do
-    (cd "$d" && ./push.sh) &
-done
-
-wait
